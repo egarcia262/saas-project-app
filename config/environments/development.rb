e85239f82b99e202dd11012e56e3b76c85112dc6
Edu <egarcia262@gmail.com>
2022-07-04 15:16:23 +0000

    Set up mail configuration in local

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,4 +58,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  
+  config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = { :host => 'https://73872c8cef6a490e9bacd11bc68697ce.vfs.cloud9.us-east-1.amazonaws.com/'}
 end
